e5b06b990b41c3c6f39ef310ad80542280d9cb0f
stewy <stewy@koanlogic.com>
2011-03-07 15:44:23 +0100

    key refresh in test

--- bindings/python/test.py
+++ bindings/python/test.py
@@ -4,27 +4,65 @@
 
 import scs
 
-s = scs.scs_init(
-        'tid', 
-        0, 
-        'k', 
-        'hk', 
-        0, 
-        0);
 
-size = 0
+# encode/decode check
+def test(str):
+    print 'input state: [', str, ']'
 
-str = 'some cool state'
-print 'input state: [', str, ']'
+    ck = scs.scs_encode(s, str, len(str))
+    if ck is None:
+        print '[err]: ', scs.scs_err(s), 
+        raise Exception
+        
+    print 'encoded cookie: [', ck, ']'
 
-ck = scs.scs_encode(s, str, len(str))
-print 'encoded cookie: [', ck, ']'
+    st = scs.scs_decode(s, ck)
+    if st is None:
+        print '[err]: ', scs.scs_err(s), 
+        raise Exception
 
-st = scs.scs_decode(s, ck)
-
-if st is None:
-    print 'err: [', scs.scs_err(s), ']'
-else:
     print 'decoded state: [', st, ']'
 
-scs.scs_term(s)
+    if st != str:
+        print '[err] input state different from decoded state!'
+        raise Exception
+
+
+# Run tests
+s = None
+
+try:
+    
+    # initialise SCS parameters
+    s = scs.scs_init(
+            'tid', 
+            0, 
+            'k', 
+            'hk', 
+            0, 
+            0);
+
+    size = 0
+
+    # basic encoding/decoding tests
+    test('some cool state 1')
+    test('some other state 2')
+
+    # test key refresh
+    scs.scs_refresh_keyset(s, 'tid2', 'k2', 'hk2')
+
+    test('some other cool state 3 after key refresh')
+
+    # cleanup
+    scs.scs_term(s)
+
+    print 'All tests passed.'
+
+except Exception:
+
+    # cleanup
+    if s != None:
+        scs.scs_term(s)
+
+    print 'Failure in tests!'
+
