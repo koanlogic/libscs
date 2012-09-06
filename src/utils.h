5d3a44ef44963d1331940cb236b397a19ec828dd
Thomas Fossati <tfossati@velocix.com>
2012-09-06 15:26:25 +0100

    [libscs] recursive header inclusion (harmless but odd)

--- src/utils.h
+++ src/utils.h
@@ -4,7 +4,6 @@
 #include "scs_conf.h"
 #include "scs.h"
 #include "scs_priv.h"
-#include "utils.h"
 
 int scs_set_error (scs_t *scs, scs_err_t rc, const char *fmt, ...);
 void debug_print_buf (const char *label, const uint8_t *b, size_t b_sz);
