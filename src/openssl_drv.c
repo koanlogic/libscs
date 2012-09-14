b919c31a54fbbba373ecf9dd1b7826671af1a846
Thomas Fossati <tfossati@velocix.com>
2012-09-14 22:49:56 +0100

    [libscs] some cleanup in the openssl driver

--- src/openssl_drv.c
+++ src/openssl_drv.c
@@ -42,7 +42,7 @@ int openssl_rand (scs_t *ctx, uint8_t *b, size_t b_sz)
 int openssl_enc (scs_t *ctx)
 {
     EVP_CIPHER_CTX c;
-    int tmp_sz, out_sz;
+    int tmp_sz, sz;
     scs_atoms_t *ats = &ctx->atoms;
     scs_keyset_t *ks = &ctx->cur_keyset;
 
@@ -55,17 +55,14 @@ int openssl_enc (scs_t *ctx)
     EVP_CIPHER_CTX_init(&c);
 
     if (!EVP_EncryptInit_ex(&c, EVP_aes_128_cbc(), NULL, ks->key, ats->iv) 
-            || !EVP_EncryptUpdate(&c, ats->data, &out_sz, ats->data, 
-                                  ats->data_sz) 
-            || !EVP_EncryptFinal_ex(&c, ats->data + out_sz, &tmp_sz) 
+            || !EVP_EncryptUpdate(&c, ats->data, &sz, ats->data, ats->data_sz) 
+            || !EVP_EncryptFinal_ex(&c, ats->data + sz, &tmp_sz) 
             || !EVP_CIPHER_CTX_cleanup(&c))
     {
         scs_set_error(ctx, SCS_ERR_CRYPTO, "openssl error: %s",
                 ERR_error_string(ERR_get_error(), NULL));
         return -1;
     }
-
-    out_sz += tmp_sz;
 
     EVP_CIPHER_CTX_cleanup(&c);
 
@@ -75,7 +72,7 @@ int openssl_enc (scs_t *ctx)
 int openssl_dec (scs_t *ctx, scs_keyset_t *ks)
 {
     EVP_CIPHER_CTX c;
-    int tmp_sz, out_sz;
+    int tmp_sz, sz;
     scs_atoms_t *ats = &ctx->atoms;
 
     if (ks->cipherset != AES_128_CBC_HMAC_SHA1)
@@ -87,17 +84,14 @@ int openssl_dec (scs_t *ctx, scs_keyset_t *ks)
     EVP_CIPHER_CTX_init(&c);
 
     if (!EVP_DecryptInit_ex(&c, EVP_aes_128_cbc(), NULL, ks->key, ats->iv) 
-            || !EVP_DecryptUpdate(&c, ats->data, &out_sz, ats->data, 
-                                  ats->data_sz) 
-            || !EVP_DecryptFinal_ex(&c, ats->data + out_sz, &tmp_sz) 
+            || !EVP_DecryptUpdate(&c, ats->data, &sz, ats->data, ats->data_sz) 
+            || !EVP_DecryptFinal_ex(&c, ats->data + sz, &tmp_sz) 
             || !EVP_CIPHER_CTX_cleanup(&c))
     {
         scs_set_error(ctx, SCS_ERR_CRYPTO, "openssl error: %s",
                 ERR_error_string(ERR_get_error(), NULL));
         return -1;
     }
-
-    out_sz += tmp_sz;
 
     EVP_CIPHER_CTX_cleanup(&c);
 
@@ -117,31 +111,32 @@ int openssl_tag (scs_t *ctx, scs_keyset_t *ks)
 
     HMAC_CTX_init(&c);
 
+    size_t edata_len = strlen(ats->b64_data);
+    size_t etstamp_len = strlen(ats->b64_tstamp);
+    size_t etid_len = strlen(ats->b64_tid);
+    size_t eiv_len = strlen(ats->b64_iv);
+
 #if OPENSSL_VERSION_NUMBER < 0x10000000L
     HMAC_Init_ex(&c, ks->hkey, ks->hkey_sz, EVP_sha1(), NULL);
 
-    HMAC_Update(&c, (unsigned char *) ats->b64_data, strlen(ats->b64_data));
+    HMAC_Update(&c, (unsigned char *) ats->b64_data, edata_len);
     HMAC_Update(&c, (unsigned char *) "|", 1);
-    HMAC_Update(&c, (unsigned char *) ats->b64_tstamp, strlen(ats->b64_tstamp));
+    HMAC_Update(&c, (unsigned char *) ats->b64_tstamp, etstamp_len);
     HMAC_Update(&c, (unsigned char *) "|", 1);
-    HMAC_Update(&c, (unsigned char *) ats->b64_tid, strlen(ats->b64_tid));
+    HMAC_Update(&c, (unsigned char *) ats->b64_tid, etid_len);
     HMAC_Update(&c, (unsigned char *) "|", 1);
-    HMAC_Update(&c, (unsigned char *) ats->b64_iv, strlen(ats->b64_iv));
+    HMAC_Update(&c, (unsigned char *) ats->b64_iv, eiv_len);
 
     HMAC_Final(&c, ats->tag, (unsigned int *) &ats->tag_sz);
 #else
     if (!HMAC_Init_ex(&c, ks->hkey, ks->hkey_sz, EVP_sha1(), NULL)
-            || !HMAC_Update(&c, (unsigned char *) ats->b64_data, 
-                            strlen(ats->b64_data))
+            || !HMAC_Update(&c, (unsigned char *) ats->b64_data, edata_len);
             || !HMAC_Update(&c, (unsigned char *) "|", 1);
-            || !HMAC_Update(&c, (unsigned char *) ats->b64_tstamp, 
-                            strlen(ats->b64_tstamp))
+            || !HMAC_Update(&c, (unsigned char *) ats->b64_tstamp, etstamp_len)
             || !HMAC_Update(&c, (unsigned char *) "|", 1);
-            || !HMAC_Update(&c, (unsigned char *) ats->b64_tid, 
-                            strlen(ats->b64_tid))
+            || !HMAC_Update(&c, (unsigned char *) ats->b64_tid, etid_len)
             || !HMAC_Update(&c, (unsigned char *) "|", 1);
-            || !HMAC_Update(&c, (unsigned char *) ats->b64_iv, 
-                            strlen(ats->b64_iv))
+            || !HMAC_Update(&c, (unsigned char *) ats->b64_iv, eiv_len)
             || !HMAC_Final(&c, ats->tag, (unsigned int *) &ats->tag_sz))
     {
         scs_set_error(ctx, SCS_ERR_CRYPTO, "openssl error: %s", 
